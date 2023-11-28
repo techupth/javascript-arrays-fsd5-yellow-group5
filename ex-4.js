2754bc078000acddea4bc72ce0982e2948a6d49e
Gun <cobaltgun2324@gmail.com>
2023-11-28 14:01:58 +0700

    feat: add complete exercise

--- ex-4.js
+++ ex-4.js
@@ -12,3 +12,13 @@ const employees = [
 ];
 
 // Start coding here
+employees.push(
+  {
+    name: "Alicia",
+    age: 29,
+    hobbies: ["Shopping", "Reading novels"],
+  },
+  { name: "Kody", age: 19, hobbies: ["Computer games", "Wakeboard"] }
+);
+
+console.log(employees);
